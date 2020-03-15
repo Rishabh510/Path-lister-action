98a67933e25f1d17db7c6d2e699457b8be586609
Rishabh Raizada <rishabh5102000@gmail.com>
2020-03-15 17:03:34 +0530

    Edit files

--- main.py
+++ main.py
@@ -7,14 +7,15 @@ def main():
     # strict = os.environ["INPUT_STRICT"] == "true"
 
     dir_path = os.path.dirname(os.path.realpath(__file__))
-    f=open("output.txt", "w+")
+    # f=open("output.txt", "w")
 
     for root, dirs, files in os.walk(dir_path):
     	for file in files:
     		if file.endswith('.yml'):
-    			f.write(root+'\\'+str(file)+"\n")
+    			print(root+'\\'+str(file)+"\n")
+    			# f.write(root+'\\'+str(file)+"\n")
 
-    f.close()
+    # f.close()
 
     sys.exit(0)
 
